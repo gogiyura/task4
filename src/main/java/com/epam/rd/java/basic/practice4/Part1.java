8f87e81be472afa343acc74d5e655685b02f8a86
gogiyura <19502507+gogiyura@users.noreply.github.com>
2021-09-26 02:24:23 +0300

    Part1 is solved

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -6,13 +6,16 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Part1 {
     private static final Logger LOGGER = Logger.getLogger(Part1.class.getName());
     public static void main(String[] args) {
         String input;
         input = getInput("part1.txt");
-        System.out.println(input);
+        System.out.println("Входные данные:\n" + input);
+        System.out.println("\nРезультаты обработки:\n" + inputProcessing(input));
     }
 
     private static String getInput(String fileName) {
@@ -30,5 +33,20 @@ public class Part1 {
             System.exit(2);
         }
         return sb.toString();
+    }
+
+    public static String inputProcessing(String input) {
+        StringBuilder sb = new StringBuilder();
+        String regex = "([\\p{L}]+)[^\\p{L}]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input + " ");
+
+        while (matcher.find()) {
+            String block = matcher.group();
+            String word = block.replaceAll(regex, "$1");
+            sb.append((word.length() >= 4 ? block.substring(2) : block));
+        }
+
+        return sb.substring(0, sb.length() - 1);
     }
 }
