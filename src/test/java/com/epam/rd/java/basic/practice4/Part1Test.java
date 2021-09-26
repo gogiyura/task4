9e44d7bd7070b84c92943e7c3c60d7660b6c15bb
gogiyura <19502507+gogiyura@users.noreply.github.com>
2021-09-26 03:07:59 +0300

    test added

--- src/test/java/com/epam/rd/java/basic/practice4/Part1Test.java
+++ src/test/java/com/epam/rd/java/basic/practice4/Part1Test.java
@@ -1,1 +1,13 @@
 package com.epam.rd.java.basic.practice4;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Part1Test {
+    @Test
+    public void testInputProcessing() {
+        String res = Part1.inputProcessing("qwerty");
+        Assert.assertTrue(res.equals("erty"));
+
+    }
+}
